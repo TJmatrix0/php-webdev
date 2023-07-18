ebf125f7b785c8ae118a73c39d5f25fce482d48e
Justin Fernandez <justinfernandez866@yahoo.com>
2023-07-18 12:15:57 -0600

    Updated Navbar & Added Navigation Paths

--- src/Controller/MatrixController.php
+++ src/Controller/MatrixController.php
@@ -8,28 +8,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MatrixController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: "app_homepage")]
     public function homePage(): Response {
         return $this->render("matrix/homePage.html.twig", [
             "title" => "Matrix Games",
         ]);
     }
 
-    #[Route("/about")]
+    #[Route("/about", name: "app_about")]
     public function about(): Response {
         return $this->render("matrix/about.html.twig", [
             "title" => "About Us",
         ]);
     }
 
-    #[Route("/giveaways")]
+    #[Route("/giveaways", name: "app_giveaways")]
     public function giveaways(): Response {
         return $this->render("matrix/giveaways.html.twig", [
             "title" => "Giveaways",
         ]);
     }
 
-    #[Route("/login")]
+    #[Route("/login", name: "app_login")]
     public function login(): Response {
         return $this->render("matrix/login.html.twig", [
             "title" => "Log In",

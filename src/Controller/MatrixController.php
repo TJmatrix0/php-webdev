c5842a9f6932a6a58462c91818adf8914ce28c47
Justin Fernandez <justinfernandez866@yahoo.com>
2023-07-18 15:12:38 -0600

    Basic Giveaway Page Layout & Design

--- src/Controller/MatrixController.php
+++ src/Controller/MatrixController.php
@@ -11,28 +11,28 @@ class MatrixController extends AbstractController
     #[Route('/', name: "app_homepage")]
     public function homePage(): Response {
         return $this->render("matrix/homePage.html.twig", [
-            "title" => "Matrix Games",
+            "header" => "Matrix Game Showcase",
         ]);
     }
 
     #[Route("/about", name: "app_about")]
     public function about(): Response {
         return $this->render("matrix/about.html.twig", [
-            "title" => "About Us",
+            "header" => "About",
         ]);
     }
 
     #[Route("/giveaways", name: "app_giveaways")]
     public function giveaways(): Response {
         return $this->render("matrix/giveaways.html.twig", [
-            "title" => "Giveaways",
+            "header" => "Giveaways",
         ]);
     }
 
     #[Route("/login", name: "app_login")]
     public function login(): Response {
         return $this->render("matrix/login.html.twig", [
-            "title" => "Log In",
+            "header" => "Log In",
         ]);
     }
 }
